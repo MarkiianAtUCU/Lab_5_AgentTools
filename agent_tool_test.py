5156d61adad9bb5297ccb3dd9f4fb3bde03cc261
Markiian <matsiuk@ucu.edu.ua>
2018-03-05 23:08:32 +0200

    commit_2

--- agent_tool_test.py
+++ agent_tool_test.py
@@ -1,6 +1,11 @@
 from agent_tool import *
+import io
+import sys
+import unittest
+
+# Preparation
 ag = Agent()
-# ag.add_property()
+
 hr = HouseRental(**{'square_feet': '20sq', 'beds': '20b', 'baths': '20ba',
                     'fenced': 'yes', 'garage': 'none', 'num_stories': '20',
                     'rent': '200',  'utilities': '20', 'furnished': 'no'})
@@ -18,4 +23,47 @@ ap = ApartmentPurchase(**{'square_feet': '20', 'beds': '1', 'baths': '2',
                           'laundry': 'ensuite', 'balcony': 'no',
                           'price': '20000', 'taxes': '10'})
 ag.property_list = [hr, hp, ar, ap]
-ag.display_properties()
+
+
+def test_capture(st):
+    """
+    (str) -> str
+
+    Function capture output from evaluation given string
+    """
+    capturedOutput = io.StringIO()
+    sys.stdout = capturedOutput
+    eval(st)
+    sys.stdout = sys.__stdout__
+    return capturedOutput.getvalue()
+
+# Test cases
+t1 = "ag.display_properties()"
+t2 = "ag.display_rent_less(1000)"
+t3 = "ag.display_price_less(1000)"
+t4 = "ag.display_price_less(20000)"
+
+r1 = 'PROPERTY DETAILS\n================\nsquare footage: 20sq\nbedrooms: 20b\nbathrooms: 20ba\n\nHOUSE DETAILS\n# of stories: 20\ngarage: none\nfenced yard: yes\nRENTAL DETAILS\nrent: 200\nestimated utilities: 20\nfurnished: no\nPROPERTY DETAILS\n================\nsquare footage: 200\nbedrooms: 1\nbathrooms: 20\n\nHOUSE DETAILS\n# of stories: 20\ngarage: none\nfenced yard: no\nPURCHASE DETAILS\nselling price: 10\nestimated taxes: 20\nPROPERTY DETAILS\n================\nsquare footage: 20\nbedrooms: 10\nbathrooms: 20\n\nAPARTMENT DETAILS\nlaundry: coin\nhas balcony : solarium\nRENTAL DETAILS\nrent: 2000\nestimated utilities: 10\nfurnished: no\nPROPERTY DETAILS\n================\nsquare footage: 20\nbedrooms: 1\nbathrooms: 2\n\nAPARTMENT DETAILS\nlaundry: ensuite\nhas balcony : no\nPURCHASE DETAILS\nselling price: 20000\nestimated taxes: 10\n'
+r2 = 'PROPERTY DETAILS\n================\nsquare footage: 20sq\nbedrooms: 20b\nbathrooms: 20ba\n\nHOUSE DETAILS\n# of stories: 20\ngarage: none\nfenced yard: yes\nRENTAL DETAILS\nrent: 200\nestimated utilities: 20\nfurnished: no\n'
+r3 = 'PROPERTY DETAILS\n================\nsquare footage: 200\nbedrooms: 1\nbathrooms: 20\n\nHOUSE DETAILS\n# of stories: 20\ngarage: none\nfenced yard: no\nPURCHASE DETAILS\nselling price: 10\nestimated taxes: 20\n'
+r4 = 'PROPERTY DETAILS\n================\nsquare footage: 200\nbedrooms: 1\nbathrooms: 20\n\nHOUSE DETAILS\n# of stories: 20\ngarage: none\nfenced yard: no\nPURCHASE DETAILS\nselling price: 10\nestimated taxes: 20\nPROPERTY DETAILS\n================\nsquare footage: 20\nbedrooms: 1\nbathrooms: 2\n\nAPARTMENT DETAILS\nlaundry: ensuite\nhas balcony : no\nPURCHASE DETAILS\nselling price: 20000\nestimated taxes: 10\n'
+
+
+# Test
+class TestStringMethods(unittest.TestCase):
+
+    def test_main(self):
+        self.assertEqual(test_capture(t1), r1)
+
+    def test_display_rent_less(self):
+        self.assertEqual(test_capture(t2), r2)
+
+    def test_display_price_less_1(self):
+        self.assertEqual(test_capture(t3), r3)
+
+    def test_display_price_less_2(self):
+        self.assertEqual(test_capture(t4), r4)
+
+
+if __name__ == '__main__':
+    unittest.main()
